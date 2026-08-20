302e28fd79750d4dc2d0b1d150ba013de000d2b3
tharu12sa <tharushakavinda621@gmail.com>
2026-08-20 14:20:25 +0530

    fix: Add login functionality with fetch request for user authentication

--- app.js
+++ app.js
@@ -65,3 +65,24 @@ function btnAddemployee() {
         })
         .catch((error) => console.error(error));
 }
+// -----------------------------------------------------------------------------
+
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+    "userName": "string",
+    "password": "string"
+});
+
+const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+};
+
+fetch("https://api.freeprojectapi.com/api/BankLoan/login", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
