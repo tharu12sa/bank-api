78a8890a150dceb8b6a3129c7a95bf2c391904c9
tharu12sa <tharushakavinda621@gmail.com>
2026-08-20 11:14:22 +0530

    feat: Update input fields in customer registration form for clarity and correctness

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ myHeaders.append("Content-Type", "application/json");
 const raw = JSON.stringify({
     "userId": document.getElementById("customerId").value,
     "userName": document.getElementById("userName").value,
-    "emailId": document.getElementById("customerId").value,
+    "emailId": document.getElementById("email").value,
     "fullName": document.getElementById("customerId").value,
     "password": document.getElementById("customerId").value
 });
