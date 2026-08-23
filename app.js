f028cab8fcdf528088bf05fe3c1e1cd0f03d4747
tharu12sa <tharushakavinda621@gmail.com>
2026-08-23 10:56:12 +0530

    feat: Implement saveLoan function and link to loan submission button

--- app.js
+++ app.js
@@ -128,3 +128,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 // -----------------------------------------------------------------------------------------------
 
+function saveLoan(){
+
+}
